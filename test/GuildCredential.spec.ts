bedb1fedec5442c17565d147dd0b6960f28adf39
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-04-21 01:09:03 +0200

    Remove outdated tests, skip the ones yet to be reworked

--- test/GuildCredential.spec.ts
+++ test/GuildCredential.spec.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { constants, Contract, ContractFactory, ContractTransaction } from "ethers";
+import { constants, Contract, ContractFactory } from "ethers";
 import { ethers, upgrades } from "hardhat";
 
 // NFT CONFIG
@@ -15,14 +15,8 @@ const cids = [
 ];
 const fee = ethers.utils.parseEther("0.1");
 
-// ORACLE CONFIG
-const jobId = "0x7599d3c8f31e4ce78ad2b790cbcfc673".padEnd(66, "0");
-const oracleFee = ethers.utils.parseEther("0.05");
-
 // CONTRACTS
 let mockERC20: Contract;
-let chainlinkToken: Contract;
-let chainlinkOperator: Contract;
 let GuildCredential: ContractFactory;
 let credential: Contract;
 
@@ -31,24 +25,10 @@ let wallet0: SignerWithAddress;
 let randomWallet: SignerWithAddress;
 let treasury: SignerWithAddress;
 
-const oracleResponse = {
-  NO_ACCESS: "0x".padEnd(66, "0"),
-  ACCESS: `${"0x".padEnd(65, "0")}1`,
-  CHECK_FAILED: `${"0x".padEnd(65, "0")}2`
-};
-
 enum GuildAction {
   JOINED_GUILD,
   IS_OWNER,
   IS_ADMIN
-}
-
-async function getRequestId(tx: ContractTransaction): Promise<string> {
-  const res = await tx.wait();
-  // Assuming the first event's name is ChainlinkRequested
-  expect(res.logs[0].topics[0]).to.eq("0xb5e6e01e79f91267dc17b4e6314d5d4d03593d2ceee0fbb452b750bd70ea5af9");
-  // The first indexed parameter of ChainlinkRequested is the requestId
-  return res.logs[0].topics[1];
 }
 
 describe("GuildCredential", () => {
@@ -58,30 +38,17 @@ describe("GuildCredential", () => {
     const ERC20 = await ethers.getContractFactory("MockERC20");
     mockERC20 = await ERC20.deploy("Mock Token", "MCK");
     mockERC20.mint(wallet0.address, ethers.utils.parseEther("100"));
-
-    const LINK = await ethers.getContractFactory("MockERC677");
-    chainlinkToken = await LINK.deploy("Link Token", "LINK");
-
-    const Operator = await ethers.getContractFactory("MockOperator");
-    chainlinkOperator = await Operator.deploy(chainlinkToken.address);
   });
 
   beforeEach("deploy contract", async () => {
     GuildCredential = await ethers.getContractFactory("GuildCredential");
-    credential = await upgrades.deployProxy(
-      GuildCredential,
-      [name, symbol, chainlinkToken.address, chainlinkOperator.address, treasury.address],
-      {
-        constructorArgs: [jobId, oracleFee],
-        kind: "uups"
-      }
-    );
+    credential = await upgrades.deployProxy(GuildCredential, [name, symbol, treasury.address], {
+      kind: "uups"
+    });
     await credential.deployed();
 
     credential.setFee(constants.AddressZero, fee);
     credential.setFee(mockERC20.address, fee);
-
-    chainlinkToken.mint(credential.address, ethers.utils.parseEther("100"));
   });
 
   it("should have initialized the state variables", async () => {
@@ -93,7 +60,6 @@ describe("GuildCredential", () => {
 
   it("should be upgradeable", async () => {
     const upgraded = await upgrades.upgradeProxy(credential.address, GuildCredential, {
-      constructorArgs: [jobId, oracleFee],
       kind: "uups"
     });
 
@@ -181,13 +147,10 @@ describe("GuildCredential", () => {
       await expect(credential.tokenURI(84)).to.revertedWithCustomError(credential, "NonExistentToken").withArgs(84);
     });
 
-    it("should return the correct tokenURI", async () => {
+    it.skip("should return the correct tokenURI", async () => {
       /* eslint-disable no-await-in-loop */
       for (let i = 0; i < cids.length; i += 1) {
-        const requestId = await getRequestId(
-          await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985 + i, cids[i], { value: fee })
-        );
-        await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+        await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985 + i, cids[i], { value: fee }); // TODO
         const regex = new RegExp(`ipfs://${cids[i]}`);
         expect(regex.test(await credential.tokenURI(i + 1))).to.eq(true);
       }
@@ -195,12 +158,10 @@ describe("GuildCredential", () => {
     });
   });
 
-  context("#claim", () => {
+  // TODO
+  context.skip("#claim", () => {
     it("fails if the address has already claimed", async () => {
-      const requestId = await getRequestId(
-        await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee })
-      );
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee });
       await expect(
         credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee })
       ).to.be.revertedWithCustomError(credential, "AlreadyClaimed");
@@ -271,60 +232,14 @@ describe("GuildCredential", () => {
         .withArgs(fee.div(2), fee);
     });
 
-    it("emits ClaimRequested event", async () => {
-      await expect(credential.claim(constants.AddressZero, GuildAction.IS_ADMIN, 1985, cids[0], { value: fee }))
-        .to.emit(credential, "ClaimRequested")
-        .withArgs(wallet0.address, GuildAction.IS_ADMIN, 1985);
-    });
-  });
-
-  context("#fulfillClaim", () => {
-    let requestId: string;
-
-    beforeEach("make a claim request", async () => {
-      requestId = await getRequestId(
-        await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee })
-      );
-    });
-
-    it("fails if the address doesn't fulfill the requirements", async () => {
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.NO_ACCESS))
-        .to.be.revertedWithCustomError(credential, "NoAccess")
-        .withArgs(wallet0.address);
-    });
-
-    it("fails if the check failed or invalid data was returned by the oracle", async () => {
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.CHECK_FAILED))
-        .to.be.revertedWithCustomError(credential, "AccessCheckFailed")
-        .withArgs(wallet0.address);
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, `${"0x".padEnd(65, "0")}3`))
-        .to.be.revertedWithCustomError(credential, "AccessCheckFailed")
-        .withArgs(wallet0.address);
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, `${"0x".padEnd(65, "0")}9`))
-        .to.be.revertedWithCustomError(credential, "AccessCheckFailed")
-        .withArgs(wallet0.address);
-    });
-
-    it("fails if the claim was already fulfilled", async () => {
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS)).to.be.revertedWith(
-        "Must have a valid requestId"
-      );
-      // Note: the above error is thrown by the Chainlink Operator contract. However, a non-official/malicious
-      // implementation might skip that check. In those cases, the below check will still prevent double fulfills:
-      // await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS)).to.be.revertedWith(
-      //   "Source must be the oracle of the request"
-      // );
-    });
-
     it("should set the address's claim status", async () => {
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      // claim
       expect(await credential.hasClaimed(wallet0.address, GuildAction.JOINED_GUILD, 1985)).to.eq(true);
     });
 
     it("should increment the total supply", async () => {
       const totalSupply0 = await credential.totalSupply();
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      // claim
       const totalSupply1 = await credential.totalSupply();
       expect(totalSupply1).to.eq(totalSupply0.add(1));
     });
@@ -334,24 +249,22 @@ describe("GuildCredential", () => {
       const tokenId = totalSupply.add(1);
       expect(await credential.balanceOf(wallet0.address)).to.eq(0);
       await expect(credential.ownerOf(tokenId)).to.be.revertedWith("ERC721: invalid token ID");
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      // claim
       expect(await credential.balanceOf(wallet0.address)).to.eq(1);
       expect(await credential.ownerOf(tokenId)).to.eq(wallet0.address);
     });
 
     it("emits Claimed event", async () => {
-      await expect(chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS))
+      await expect(credential.claim(mockERC20.address, GuildAction.IS_ADMIN, 1985, cids[0]))
         .to.emit(credential, "Claimed")
         .withArgs(wallet0.address, GuildAction.JOINED_GUILD, 1985);
     });
   });
 
-  context("#burn", () => {
+  // TODO
+  context.skip("#burn", () => {
     beforeEach("claim a token", async () => {
-      const requestId = await getRequestId(
-        await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee })
-      );
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee });
     });
 
     it("should reset hasClaimed to false", async () => {
@@ -374,12 +287,10 @@ describe("GuildCredential", () => {
     });
   });
 
-  context("#updateTokenURI", () => {
+  // TODO
+  context.skip("#updateTokenURI", () => {
     beforeEach("claim a token", async () => {
-      const requestId = await getRequestId(
-        await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee })
-      );
-      await chainlinkOperator.tryFulfillOracleRequest(requestId, oracleResponse.ACCESS);
+      await credential.claim(constants.AddressZero, GuildAction.JOINED_GUILD, 1985, cids[0], { value: fee });
     });
 
     it("fails if the token is not yet minted", async () => {
